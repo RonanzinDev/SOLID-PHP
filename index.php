cbdd5167413b27b4bd2b00a2c0ebc20c411ecf5b
Ronanzindev <morellianogm@gmail.com>
2022-08-17 20:57:50 -0300

    A lot more things

--- index.php
+++ index.php
@@ -4,8 +4,31 @@ require __DIR__ . "/vendor/autoload.php";
 use App\CarrinhoCompra;
 
 $carrinho1 = new CarrinhoCompra();
-$carrinho1->adicionarItem("Bicicleta", 1000);
-$carrinho1->adicionarItem("Carro", 20000);
 
-$carrinho1->exibirValorTotal();
 print_r($carrinho1->exibirItens());
+echo 'Valor total: ' . $carrinho1->exibirValorTotal();
+
+/*
+$carrinho1->adicionarItem('Bicicleta', 750.10);
+$carrinho1->adicionarItem('Galadeira', 1950.15);
+$carrinho1->adicionarItem('Tapete', 350.20);
+*/
+
+echo "<br />";
+print_r($carrinho1->exibirItens());
+echo 'Valor total recalculado: ' . $carrinho1->exibirValorTotal();
+
+echo "<br />";
+echo 'status: ' . $carrinho1->exibirStatus();
+
+
+$carrinho1->adicionarItem('Televisão 65"', 3570.25);
+
+echo "<br />";
+if ($carrinho1->confirmarPedido()) {
+    echo 'Pedido realizado com sucesso!';
+} else {
+    echo 'Erro na confirmação do pedido. Carrinho não possui itens';
+}
+echo "<br />";
+echo 'status: ' . $carrinho1->exibirStatus();
